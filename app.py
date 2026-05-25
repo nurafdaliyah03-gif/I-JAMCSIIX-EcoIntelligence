fd6fa453ff5f83dd02dd460a39224ca5ca767604
nurafdaliyah03-gif <nurafdaliyah03@gmail.com>
2026-05-25 18:05:56 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-print(df.head())
-print(df.dtypes)
-st.session_state.df = df
+if 'df' not in st.session_state: 
+    url = "https://raw.githubusercontent.com/nurafdaliyah03-gif/I-JAMCSIIX-EcoIntelligence/refs/heads/main/data_jamsicx.csv"
+    
+    df = pd.read_csv(url)
+
+    df.columns = df.columns.str.strip()
+    df['PROVINSI'] = df['PROVINSI'].astype(str).str.strip().str.upper()
+
+    st.session_state.df = df
+
+
+# CEK STRUKTUR DATA (SEMENTARA)
 st.subheader("Cek Struktur Data")
 
 st.write("5 Baris Awal:")
-st.dataframe(df.head())
+st.dataframe(st.session_state.df.head())
 
 st.write("Tipe Data:")
-st.write(df.dtypes.astype(str))
+st.write(st.session_state.df.dtypes.astype(str))
