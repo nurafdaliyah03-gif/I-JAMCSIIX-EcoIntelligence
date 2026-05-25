bfebc416da61ae133412412fc330ccb43ff7a92e
nurafdaliyah03-gif <nurafdaliyah03@gmail.com>
2026-05-25 18:03:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,10 @@
 print(df.head())
 print(df.dtypes)
+st.session_state.df = df
+st.subheader("Cek Struktur Data")
+
+st.write("5 Baris Awal:")
+st.dataframe(df.head())
+
+st.write("Tipe Data:")
+st.write(df.dtypes.astype(str))
